deec3f2692e16c86ad1d322b186d73a4cc16086f
8184993698 <hareeshbandi99@gmail.com>
2024-01-30 21:54:31 +0530

    added dto changes

--- src/main/java/com/gittestdemo/GitTestAppApplication.java
+++ src/main/java/com/gittestdemo/GitTestAppApplication.java
@@ -2,9 +2,20 @@ package com.gittestdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GitTestAppApplication {
+	@PostMapping("/purchase")
+	public String purchase(@RequestBody Order order) {
+		return "Hi "+order.getUserame()+" order for "+order.getProductname()
+		+" amount with "+order.getAmount()+" stored succesfully.....";
+		
+	};
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitTestAppApplication.class, args);
